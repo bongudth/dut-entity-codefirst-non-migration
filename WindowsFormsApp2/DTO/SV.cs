9d650efb9e9a68cb2056b5aa2bdb7e83e914c555
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-07-22 11:30:13 +0700

    :package: add SV.cs and LSH.cs to DTO

---
+++ WindowsFormsApp2/DTO/SV.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2.DTO
+{
+    class SV
+    {
+        [Key]
+        public string MSSV { get; set; }
+        public string NameSV { get; set; }
+        public int ID_Lop { get; set; }
+    }
+}
